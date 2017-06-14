e97c84b4aabd11beac2f0d85568104f6dd5a145e
陆家林 <3140102299@zju.edu.cn>
2017-06-14 20:15:54 +0800

    tf train and test okay

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from __future__ import absolute_import
 from __future__ import division
-from __future__ import print_function
+from pprint import pprint as pr
 
 import argparse
 import sys
-
 
 import tensorflow as tf
 
@@ -14,16 +13,13 @@ FLAGS = None
 
 
 def main(_):
-    # Import data
-    mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
-
     model=models.new_model(FLAGS.framework)
-
+    model.train()
+    model.test()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data_dir', type=str, default='/tmp/tensorflow/mnist/input_data',
-                      help='Directory for storing input data')
-    parser.add_argument('--framework', type=str, default='tensorflow',
+    parser.add_argument('--data_dir', type=str, default='/tmp/tensorflow/mnist/input_data', help='Directory for storing input data')
+    parser.add_argument('--framework', type=str, default='tensorflow', help='frame work name \'tensorflow\' \'caffe\' or \'pytorch\' ')
     FLAGS, unparsed = parser.parse_known_args()
     tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
--- models.py
+++ models.py
@@ -2,6 +2,8 @@ import tensorflow as tf
 
 from tensorflow.examples.tutorials.mnist import input_data
 
+from tqdm import tqdm
+
 def weight_variable(shape):
     initial = tf.truncated_normal(shape, stddev=0.1)
     return tf.Variable(initial)
@@ -42,48 +44,59 @@ def tf_LeNet(x,y_):
     h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
     h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
-    # add dropout
-    keep_prob = tf.placeholder(tf.float32)
-    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-
     # visible layer:output
     W_fc2 = weight_variable([1024, 10])
     b_fc2 = bias_variable([10])
 
     # output
-    y_output = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+    y_output = tf.matmul(h_fc1, W_fc2) + b_fc2
 
     cross_entropy = tf.reduce_mean(
     tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_output))
 
     return y_output,cross_entropy
 
-class Model:
-    def __init__(self,framework_name,mnist=mnist):
-        self.model=tensorflow_model()
-        self.mnist=mnist
+def new_model(type,data_dir="/tmp/tensorflow/mnist/input_data"):
+    if(type=="tensorflow"):
+        return TF_Model(data_dir)
 
+class Model():
+    def __init__(self):
+        pass
+
+    def save_model();
+        pass
+
+    def load_model():
+        pass
+
+class TF_Model(Model):
+    def __init__(self,data_dir="/tmp/tensorflow/mnist/input_data"):
+        self.data_set = input_data.read_data_sets(data_dir, one_hot=True)
+
+        self.model=self.build_model()
         self.sess = tf.InteractiveSession()
         tf.global_variables_initializer().run()
 
-    def tensorflow_model(self):
+    def build_model(self):
         # Create the model
         self.x = tf.placeholder(tf.float32, [None, 784])
-
         # Define loss and optimizer
         self.y_ = tf.placeholder(tf.float32, [None, 10])
-        self.y_output,objective_function=tf_LeNet(x,y_)
+        self.y_output,self.objective_function=tf_LeNet(self.x,self.y_)
         self.train_step = tf.train.GradientDescentOptimizer(0.5).minimize(self.objective_function)
 
-    def train(self):
+    def train(self,epoch_num=100):
         # Train
-        for _ in range(1000):
-            batch_xs, batch_ys = self.mnist.train.next_batch(100)
-            self.sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+        print("training: epoch number",epoch_num)
+        for _ in tqdm(range(epoch_num)):
+            batch_xs, batch_ys = self.data_set.train.next_batch(100)
+            # self.sess.run(self.train_step, feed_dict={self.x: batch_xs, self.y_: batch_ys,self.keep_prob:0.1})
+            self.sess.run(self.train_step, feed_dict={self.x: batch_xs, self.y_: batch_ys})
+        # self.save_model("tensorflow_model.h5py")
 
     def test(self):
         # Test trained model
-        self.correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-        self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-        print(sess.run(accuracy, feed_dict={x: mnist.test.images,
-                                          y_: mnist.test.labels}))
+        self.correct_prediction = tf.equal(tf.argmax(self.y_output, 1), tf.argmax(self.y_, 1))
+        self.accuracy = tf.reduce_mean(tf.cast(self.correct_prediction, tf.float32))
+        print("accuracy is ",self.sess.run(self.accuracy, feed_dict={self.x: self.data_set.test.images, self.y_: self.data_set.test.labels}))
